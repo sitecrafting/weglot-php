01b66f8a8d8b736050a0a8a93e5040ba909bc2bf
Baptiste Leduc <baptiste@weglot.com>
2018-04-06 12:04:39 +0200

    adding Guzzle connector

--- src/Client/Client.php
+++ src/Client/Client.php
@@ -16,12 +16,21 @@
 
 namespace Weglot\Client;
 
+use GuzzleHttp\Client as GuzzleClient;
+
 class Client
 {
     /**
      * @var array
      */
     protected $options;
+
+    /**
+     * Http connector
+     *
+     * @var GuzzleClient
+     */
+    protected $connector;
 
     /**
      * Client constructor.
@@ -32,6 +41,20 @@ class Client
     {
         // merging default options with user options
         $this->options = array_merge($this->defaultOptions(), $options);
+
+        $this->connector = $this->setupConnector();
+    }
+
+    /**
+     * Creating Guzzle HTTP connector based on $options
+     *
+     * @return GuzzleClient
+     */
+    protected function setupConnector()
+    {
+        return new GuzzleClient([
+            'base_uri' => $this->options['host']
+        ]);
     }
 
     /**
@@ -39,7 +62,7 @@ class Client
      *
      * @return array
      */
-    public function defaultOptions()
+    protected function defaultOptions()
     {
         return [
             'host'  => 'https://api.weglot.com',
