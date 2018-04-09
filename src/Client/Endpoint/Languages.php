469ef2435961d3d82e33b2818bacdfd614e17d5b
Baptiste Leduc <baptiste@weglot.com>
2018-04-09 18:23:42 +0200

    base communication for languages endpoint

---
+++ src/Client/Endpoint/Languages.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bleduc
+ * Date: 06/04/2018
+ * Time: 16:28
+ */
+
+namespace Weglot\Client\Endpoint;
+
+use Weglot\Client\Api\Exception\InvalidLanguageException;
+use Weglot\Client\Client;
+use Weglot\Client\Factory\Languages as LanguagesFactory;
+
+class Languages extends Endpoint
+{
+    const METHOD = 'GET';
+    const ENDPOINT = '/languages';
+
+    /**
+     * @var string
+     */
+    protected $iso_639_1;
+
+    /**
+     * Languages constructor.
+     * @param string $iso_639_1     ISO 639-1 code to identify language
+     * @param Client $client
+     */
+    public function __construct($iso_639_1, Client $client)
+    {
+        $this->setIso639($iso_639_1);
+        parent::__construct($client);
+    }
+
+    /**
+     * @param $iso_639_1
+     * @return $this
+     */
+    public function setIso639($iso_639_1)
+    {
+        $this->iso_639_1 = $iso_639_1;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIso639()
+    {
+        return $this->iso_639_1;
+    }
+
+    /**
+     * @return array
+     * @throws InvalidLanguageException
+     */
+    public function handle()
+    {
+        $data = LanguagesFactory::data();
+        if (!isset($data[$this->iso_639_1])) {
+            throw new InvalidLanguageException();
+        }
+        $language = $data[$this->iso_639_1];
+
+        return $language;
+    }
+}
