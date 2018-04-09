d0759235a30df289bac018437a7c70b233bb94d1
Baptiste Leduc <baptiste@weglot.com>
2018-04-09 12:15:16 +0200

    fixing exception default message

--- src/Client/Api/Exception/MissingRequiredParamException.php
+++ src/Client/Api/Exception/MissingRequiredParamException.php
@@ -8,6 +8,12 @@
 
 namespace Weglot\Client\Api\Exception;
 
+use Throwable;
+
 class MissingRequiredParamException extends \Exception
 {
+    public function __construct($message = 'Required fields for $params are: language_from, language_to, bot, request_url', $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
