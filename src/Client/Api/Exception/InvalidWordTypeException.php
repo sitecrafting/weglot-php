7a8829dff1919d0255bc8b9276d70a385c8d3900
Baptiste Leduc <baptiste@weglot.com>
2018-04-09 18:14:15 +0200

    updating exception codes

--- src/Client/Api/Exception/InvalidWordTypeException.php
+++ src/Client/Api/Exception/InvalidWordTypeException.php
@@ -12,7 +12,7 @@ use Throwable;
 
 class InvalidWordTypeException extends \Exception
 {
-    public function __construct($message = 'The given WordType is invalid', $code = 0, Throwable $previous = null)
+    public function __construct($message = 'The given WordType is invalid', $code = 400, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
--- src/Client/Api/Exception/MissingRequiredParamException.php
+++ src/Client/Api/Exception/MissingRequiredParamException.php
@@ -12,7 +12,7 @@ use Throwable;
 
 class MissingRequiredParamException extends \Exception
 {
-    public function __construct($message = 'Required fields for $params are: language_from, language_to, bot, request_url', $code = 0, Throwable $previous = null)
+    public function __construct($message = 'Required fields for $params are: language_from, language_to, bot, request_url', $code = 400, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
