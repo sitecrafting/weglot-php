044df490d1163d89aa86e7c5cc3ee7b334b6e830
Baptiste Leduc <baptiste@weglot.com>
2018-04-11 17:38:33 +0200

    adding DotEnv

--- examples/parsing-web-page/run.php
+++ examples/parsing-web-page/run.php
@@ -8,8 +8,12 @@ use Weglot\Parser\ConfigProvider\ServerConfigProvider;
 use Weglot\Parser\ConfigProvider\ManualConfigProvider;
 use Weglot\Client\Api\Enum\BotType;
 
+// DotEnv
+$dotenv = new \Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
 // Url to parse
-$url = 'https://weglot.com/documentation/getting-started';
+$url = 'https://staging.emmystore.com/products/id225854';
 
 // Config with $_SERVER variables
 $_SERVER['SERVER_NAME'] = 'weglot.com';
@@ -26,12 +30,13 @@ $config = new ManualConfigProvider($url, BotType::HUMAN);
 // Fetching url content
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 $content = curl_exec($ch);
 curl_close($ch);
 
 // Client
 $client = new Client(getenv('WG_API_KEY'));
-$parser = new Parser($client, 'en', 'fr', $config);
+$parser = new Parser($client, 'fi', 'en', $config);
 
 // Run the Parser
 $translatedContent = $parser->translate($content);
