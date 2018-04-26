45b1dfbf66b9fe36e0dec4a32f2b76e3d0781d15
Baptiste Leduc <baptiste@weglot.com>
2018-04-26 15:44:04 +0200

    improving complexity

--- src/Parser/Formatter/DomFormatter.php
+++ src/Parser/Formatter/DomFormatter.php
@@ -16,30 +16,45 @@ class DomFormatter extends AbstractFormatter
      */
     public function handle(array $nodes)
     {
-        $words = $this->getTranslated()->getInputWords();
         $translated_words = $this->getTranslated()->getOutputWords();
 
         for ($i = 0; $i < count($nodes); ++$i) {
             $currentNode = $nodes[$i];
-            $property = $currentNode['property'];
 
             if ($translated_words[$i] !== null) {
-                $current_translated = $translated_words[$i]->getWord();
+                $currentTranslated = $translated_words[$i]->getWord();
 
-                if ($currentNode['class'] instanceof MetaContent) {
-                    $currentNode['node']->$property = htmlspecialchars($current_translated);
-                } else {
-                    $currentNode['node']->$property = $current_translated;
-                }
+                $this->metaContent($currentNode, $currentTranslated);
+                $this->imageSource($currentNode, $currentTranslated, $i);
+            }
+        }
+    }
 
-                if ($currentNode['class'] instanceof ImageSource) {
-                    $currentNode['node']->src = $current_translated;
-                    if ($currentNode['node']->hasAttribute('srcset') &&
-                        $currentNode['node']->srcset != '' &&
-                        $current_translated != $words[$i]->getWord()) {
-                        $currentNode['node']->srcset = '';
-                    }
-                }
+    /**
+     * @param array $details
+     * @param string $translated
+     */
+    protected function metaContent(array $details, $translated)
+    {
+        $property = $details['property'];
+
+        if ($details['class'] instanceof MetaContent) {
+            $details['node']->$property = htmlspecialchars($translated);
+        } else {
+            $details['node']->$property = $translated;
+        }
+    }
+
+    protected function imageSource(array $details, $translated, $index)
+    {
+        $words = $this->getTranslated()->getInputWords();
+
+        if ($details['class'] instanceof ImageSource) {
+            $details['node']->src = $translated;
+            if ($details['node']->hasAttribute('srcset') &&
+                $details['node']->srcset != '' &&
+                $translated != $words[$index]->getWord()) {
+                $details['node']->srcset = '';
             }
         }
     }
