994eaa6e9cef7eeb8c8331cc09ec010d41cb50a5
Baptiste Leduc <baptiste@weglot.com>
2018-06-25 16:59:55 +0200

    updating parser.init with new abstraction

--- src/Parser/Event/ParserInitEvent.php
+++ src/Parser/Event/ParserInitEvent.php
@@ -2,17 +2,11 @@
 
 namespace Weglot\Parser\Event;
 
-use Symfony\Component\EventDispatcher\Event;
 use Weglot\Parser\Parser;
 
-class ParserInitEvent extends Event
+class ParserInitEvent extends AbstractEvent
 {
     const NAME = 'parser.init';
-
-    /**
-     * @var Parser
-     */
-    protected $parser;
 
     /**
      * ParserInitEvent constructor.
@@ -20,14 +14,6 @@ class ParserInitEvent extends Event
      */
     public function __construct(Parser $parser)
     {
-        $this->parser = $parser;
-    }
-
-    /**
-     * @return Parser
-     */
-    public function getParser()
-    {
-        return $this->parser;
+        parent::__construct($parser);
     }
 }
