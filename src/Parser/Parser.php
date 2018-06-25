f2f7c4383ebccfd1f4227fba0f46c13d26186e1e
Baptiste Leduc <baptiste@weglot.com>
2018-06-25 16:25:03 +0200

    adding usual accessor & mutators

--- src/Parser/Parser.php
+++ src/Parser/Parser.php
@@ -12,11 +12,86 @@ use Weglot\Parser\ConfigProvider\ConfigProviderInterface;
 class Parser implements ParserInterface
 {
     /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var ConfigProviderInterface
+     */
+    protected $configProvider;
+
+    /**
+     * @var array
+     */
+    protected $excludeBlocks = [];
+
+    /**
      * {@inheritdoc}
      */
-    public function __construct(Client $client, ConfigProviderInterface $config, array $excludeBlocks = [])
+    public function __construct(Client $client, ConfigProviderInterface $configProvider, array $excludeBlocks = [])
     {
-        //
+        $this
+            ->setClient($client)
+            ->setConfigProvider($configProvider)
+            ->setExcludeBlocks($excludeBlocks);
+    }
+
+    /**
+     * @param Client $client
+     * @return $this
+     */
+    public function setClient(Client $client)
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param ConfigProviderInterface $configProvider
+     * @return $this
+     */
+    public function setConfigProvider(ConfigProviderInterface $configProvider)
+    {
+        $this->configProvider = $configProvider;
+
+        return $this;
+    }
+
+    /**
+     * @return ConfigProviderInterface
+     */
+    public function getConfigProvider()
+    {
+        return $this->configProvider;
+    }
+
+    /**
+     * @param array $excludeBlocks
+     * @return $this
+     */
+    public function setExcludeBlocks(array $excludeBlocks = [])
+    {
+        $this->excludeBlocks = $excludeBlocks;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getExcludeBlocks()
+    {
+        return $this->excludeBlocks;
     }
 
     /**
