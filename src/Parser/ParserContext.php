d6cc06bae0ba2dafa9ec833e5399aa8efee85cde
Baptiste Leduc <baptiste@weglot.com>
2018-06-26 11:24:08 +0200

    adding context for the Parser during translation process

---
+++ src/Parser/ParserContext.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Weglot\Parser;
+
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Class ParserContext
+ * @package Weglot\Parser
+ */
+class ParserContext
+{
+    /**
+     * @var Parser
+     */
+    protected $parser;
+
+    /**
+     * @var string
+     */
+    protected $languageFrom;
+
+    /**
+     * @var string
+     */
+    protected $languageTo;
+
+    /**
+     * @var string
+     */
+    protected $source;
+
+    /**
+     * @var Crawler
+     */
+    protected $crawler;
+
+    /**
+     * ParserContext constructor.
+     * @param Parser $parser
+     * @param string $languageFrom
+     * @param string $languageTo
+     * @param string $source
+     */
+    public function __construct(Parser $parser, $languageFrom, $languageTo, $source)
+    {
+        $this->parser = $parser;
+        $this->languageFrom = $languageFrom;
+        $this->languageTo = $languageTo;
+        $this->source = $source;
+    }
+
+    /**
+     * @return Parser
+     */
+    public function getParser()
+    {
+        return $this->parser;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguageFrom()
+    {
+        return $this->languageFrom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguageTo()
+    {
+        return $this->languageTo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    /**
+     * @param Crawler $crawler
+     * @return $this
+     */
+    public function setCrawler(Crawler $crawler)
+    {
+        $this->crawler = $crawler;
+
+        return $this;
+    }
+
+    /**
+     * @return Crawler
+     */
+    public function getCrawler()
+    {
+        return $this->crawler;
+    }
+}
