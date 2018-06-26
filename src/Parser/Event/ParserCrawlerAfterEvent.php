f78eda524b62a6dba9e5ce22b82960ef70af14aa
Baptiste Leduc <baptiste@weglot.com>
2018-06-26 11:31:42 +0200

    removing useless crawler parameter

--- src/Parser/Event/ParserCrawlerAfterEvent.php
+++ src/Parser/Event/ParserCrawlerAfterEvent.php
@@ -2,34 +2,7 @@
 
 namespace Weglot\Parser\Event;
 
-use Symfony\Component\DomCrawler\Crawler;
-use Weglot\Parser\ParserContext;
-
 class ParserCrawlerAfterEvent extends AbstractEvent
 {
     const NAME = 'parser.crawler.after';
-
-    /**
-     * @var Crawler
-     */
-    protected $crawler;
-
-    /**
-     * ParserCrawlerAfterEvent constructor.
-     * @param ParserContext $context
-     * @param Crawler $crawler
-     */
-    public function __construct(ParserContext $context, Crawler $crawler)
-    {
-        parent::__construct($context);
-        $this->crawler = $crawler;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCrawler()
-    {
-        return $this->crawler;
-    }
 }
