716c87e57ee358bfce147f667938d78a20a4354d
Baptiste Leduc <baptiste@weglot.com>
2018-06-26 11:34:12 +0200

    fixing parser.init event & removing ParserContext constraint

--- src/Parser/Event/ParserInitEvent.php
+++ src/Parser/Event/ParserInitEvent.php
@@ -2,7 +2,32 @@
 
 namespace Weglot\Parser\Event;
 
-class ParserInitEvent extends AbstractEvent
+use Symfony\Component\EventDispatcher\Event;
+use Weglot\Parser\Parser;
+
+class ParserInitEvent extends Event
 {
     const NAME = 'parser.init';
+
+    /**
+     * @var Parser
+     */
+    protected $parser;
+
+    /**
+     * AbstractEvent constructor.
+     * @param Parser $parser
+     */
+    public function __construct(Parser $parser)
+    {
+        $this->parser = $parser;
+    }
+
+    /**
+     * @return Parser
+     */
+    public function getParser()
+    {
+        return $this->parser;
+    }
 }
