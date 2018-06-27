42f8f352911d65330ae5ee7139b353d03b52bc29
Baptiste Leduc <baptiste@weglot.com>
2018-06-27 15:37:21 +0200

    cleaning listener

--- src/Parser/Listener/DomInputDataListener.php
+++ src/Parser/Listener/DomInputDataListener.php
@@ -22,7 +22,7 @@ class DomInputDataListener
 
         $nodes = $crawler->filterXPath('//input[(@type = \'submit\' or @type = \'button\') and not(ancestor-or-self::*[@' .Parser::ATTRIBUTE_NO_TRANSLATE. '])]/@*[name()=\'data-value\' or name()=\'data-order_button_text\']');
         foreach ($nodes as $node) {
-            $text = trim($node->textContent);
+            $text = trim($node->value);
             if ($text !== '') {
                 $event->getContext()->addWord($text, $node->getNodePath(), function (\DOMAttr $node, $translated) {
                     $node->value = $translated;
