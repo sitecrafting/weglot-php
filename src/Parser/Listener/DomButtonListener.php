bb5ec89c9b8c1225d3117a0575a13fe21e431af1
Baptiste Leduc <baptiste@weglot.com>
2018-06-27 11:55:55 +0200

    adding new DomButton listener

---
+++ src/Parser/Listener/DomButtonListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Weglot\Parser\Listener;
+
+use Weglot\Client\Api\Enum\WordType;
+use Weglot\Client\Api\Exception\InvalidWordTypeException;
+use Weglot\Parser\Event\ParserCrawlerAfterEvent;
+use Weglot\Parser\Exception\ParserContextException;
+use Weglot\Parser\Parser;
+
+class DomButtonListener
+{
+    /**
+     * @param ParserCrawlerAfterEvent $event
+     *
+     * @throws InvalidWordTypeException
+     * @throws ParserContextException
+     */
+    public function __invoke(ParserCrawlerAfterEvent $event)
+    {
+        $crawler = $event->getContext()->getCrawler();
+
+        $nodes = $crawler->filterXPath('//*[(self::button or self::input[@type=\'submit\']) and not(ancestor-or-self::*[@' .Parser::ATTRIBUTE_NO_TRANSLATE. '])]/@value');
+        foreach ($nodes as $node) {
+            $text = trim($node->textContent);
+            if ($text !== '') {
+                $event->getContext()->addWord($text, $node->getNodePath(), function (\DOMAttr $node, $translated) {
+                    $node->value = $translated;
+                }, WordType::VALUE);
+            }
+        }
+    }
+}
