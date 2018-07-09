1279086094dc08e39cdbaa69e2f140aef48975a5
Baptiste Leduc <baptiste@weglot.com>
2018-07-09 16:57:08 +0200

    unit testing doctypes

--- tests/unit/Parser/CrawlerTest.php
+++ tests/unit/Parser/CrawlerTest.php
@@ -72,4 +72,43 @@ class CrawlerTest extends \Codeception\Test\Unit
         $translated = $this->parser->translate('<html lang="en" prefix="og: http://ogp.me/ns#"><head><title>Test</title></head></html>', 'fr', 'en');
         $this->assertEquals('<html lang="en" prefix="og: http://ogp.me/ns#"><head><title>Test</title></head></html>', $translated);
     }
+
+    public function testDoctype()
+    {
+        // HTML 5
+        $translated = $this->parser->translate('<!DOCTYPE html><html><body><p>Test</p></body></html>', 'fr', 'en');
+        $this->assertEquals('<!DOCTYPE html><html><body><p>Test</p></body></html>', str_replace("\n", '', $translated));
+        $translated = $this->parser->translate('<!DOCTYPE html><html><head><title>Test</title></head><body><p>Test</p></body></html>', 'fr', 'en');
+        $this->assertEquals('<!DOCTYPE html><html><head><title>Test</title></head><body><p>Test</p></body></html>', str_replace("\n", '', $translated));
+        $translated = $this->parser->translate('<!DOCTYPE html><html><head><title>Test</title></head></html>', 'fr', 'en');
+        $this->assertEquals('<!DOCTYPE html><html><head><title>Test</title></head></html>', $translated);
+
+        // HTML 4.01 Strict
+        $translated = $this->parser->translate('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"><html><body><p>Test</p></body></html>', 'fr', 'en');
+        $this->assertEquals('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"><html><body><p>Test</p></body></html>', $translated);
+
+        // HTML 4.01 Transitional
+        $translated = $this->parser->translate('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"><html><body><p>Test</p></body></html>', 'fr', 'en');
+        $this->assertEquals('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"><html><body><p>Test</p></body></html>', $translated);
+
+        // HTML 4.01 Frameset
+        $translated = $this->parser->translate('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd"><html><body><p>Test</p></body></html>', 'fr', 'en');
+        $this->assertEquals('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd"><html><body><p>Test</p></body></html>', $translated);
+
+        // xHTML 1.0 Strict
+        $translated = $this->parser->translate('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html><body><p>Test</p></body></html>', 'fr', 'en');
+        $this->assertEquals('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html><body><p>Test</p></body></html>', $translated);
+
+        // xHTML 1.0 Transitional
+        $translated = $this->parser->translate('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html><body><p>Test</p></body></html>', 'fr', 'en');
+        $this->assertEquals('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html><body><p>Test</p></body></html>', $translated);
+
+        // xHTML 1.0 Frameset
+        $translated = $this->parser->translate('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd"><html><body><p>Test</p></body></html>', 'fr', 'en');
+        $this->assertEquals('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd"><html><body><p>Test</p></body></html>', $translated);
+
+        // xHTML 1.1
+        $translated = $this->parser->translate('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"><html><body><p>Test</p></body></html>', 'fr', 'en');
+        $this->assertEquals('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"><html><body><p>Test</p></body></html>', $translated);
+    }
 }
