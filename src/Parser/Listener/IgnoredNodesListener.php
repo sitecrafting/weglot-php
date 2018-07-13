9c39265c87ed15d51287b83b9699302430ee9cd8
Baptiste Leduc <baptiste@weglot.com>
2018-07-13 14:23:13 +0200

    cleaning IgnoredNodesListener

--- src/Parser/Listener/IgnoredNodesListener.php
+++ src/Parser/Listener/IgnoredNodesListener.php
@@ -35,21 +35,17 @@ final class IgnoredNodesListener
 
         // time for the BIG regex ...
         $pattern = '#<(?<tag>' .implode('|', $this->ignoredNodes). ')(?<more>\s.*?)?\>(?<content>[^>]+?)\<\/(?<tagclosed>' .implode('|', $this->ignoredNodes). ')>#i';
-
-        $last = null;
         $matches = [];
 
         // Using while instead of preg_match_all is the key to handle nested ignored nodes.
-        while (preg_match($pattern, $source, $matches) && $last !== $matches[0]) {
-            if ($matches[0] !== '' && $matches['tag'] === $matches['tagclosed']) {
+        while (preg_match($pattern, $source, $matches)) {
+            if ($matches[0] !== '') {
                 $source = str_replace(
                     $matches[0],
                     '&lt;' .$matches['tag'].str_replace('>', '&gt;', str_replace('<', '&lt;', $matches['more'])). '&gt;' . $matches['content']. '&lt;/' . $matches['tag'] . '&gt;',
                     $source
                 );
             }
-
-            $last = $matches[0];
         }
 
         $event->getContext()->setSource($source);
